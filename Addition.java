a9b9b1927a9d927e4e8fc22f1f92482cee632c20
sgbshweta <sgbshweta@gmail.com>
2023-06-12 13:41:14 +0530

    additon in java

---
+++ Addition.java
@@ -0,0 +1,15 @@
+package interface_examples;
+
+public class Addition {
+	public static void main(String[] args) {
+		
+	
+int n1=25;
+int n2=50;
+int sum;
+
+sum=n1+n2;
+System.out.println(sum);
+
+}
+}
